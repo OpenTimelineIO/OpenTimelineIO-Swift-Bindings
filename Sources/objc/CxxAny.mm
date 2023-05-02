2e063b5a11f0d0e98592d3084430e80364032c3a
Julian Yu-Chung Chen <jchen@pixar.com>
2023-05-02 09:54:26 -0700

    Use std::type_index for map index instead
    This fixes the added metadata reading unit test in Apple Silicon build.

--- Sources/objc/CxxAny.mm
+++ Sources/objc/CxxAny.mm
@@ -7,6 +7,7 @@
 
 #import "CxxAny.h"
 #import "CxxAnyDictionaryMutationStamp.h"
+#include <typeindex>
 #include <opentimelineio/stringUtils.h>
 #include <opentimelineio/serializableObject.h>
 
@@ -48,55 +49,55 @@ otio::any cxx_any_to_otio_any(CxxAny const& cxxAny) {
 
 namespace {
 struct _ToCxxAny {
-    std::map<std::type_info const*, std::function<void (otio::any const&, CxxAny*)>> function_map;
+    std::map<std::type_index, std::function<void (otio::any const&, CxxAny*)>> function_map;
     
     _ToCxxAny() {
         auto& m = function_map;
-        m[&typeid(void)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(void))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::NONE;
             
         };
-        m[&typeid(bool)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(bool))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::BOOL_;
             cxxAny->value.b = otio::any_cast<bool>(a);
         };
-        m[&typeid(int)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(int))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::INT;
             cxxAny->value.i = otio::any_cast<int>(a);
         };
-        m[&typeid(int64_t)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(int64_t))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::INT;
             cxxAny->value.i = otio::any_cast<int64_t>(a);
         };
-        m[&typeid(double)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(double))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::DOUBLE;
             cxxAny->value.d = otio::any_cast<double>(a);
         };
-        m[&typeid(std::string)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(std::string))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::STRING;
             cxxAny->value.s = otio::any_cast<std::string const&>(a).c_str();
         };
-        m[&typeid(otio::RationalTime)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::RationalTime))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::RATIONAL_TIME;
             cxxAny->value.rt = *((CxxRationalTime*)&otio::any_cast<otio::RationalTime const&>(a));
         };
-        m[&typeid(otio::TimeRange)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::TimeRange))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::TIME_RANGE;
             cxxAny->value.tr = *((CxxTimeRange*)&otio::any_cast<otio::TimeRange const&>(a));
         };
-        m[&typeid(otio::TimeTransform)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::TimeTransform))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::TIME_TRANSFORM;
             cxxAny->value.tt = *((CxxTimeTransform*)&otio::any_cast<otio::TimeTransform const&>(a));
         };
-        m[&typeid(otio::SerializableObject::Retainer<>)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::SerializableObject::Retainer<>))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::SERIALIZABLE_OBJECT;
             cxxAny->value.ptr = otio::any_cast<otio::SerializableObject::Retainer<> const&>(a).value;
         };
-        m[&typeid(otio::AnyVector)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::AnyVector))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::VECTOR;
             cxxAny->value.ptr = (void*) &otio::any_cast<otio::AnyVector const&>(a);
         };
-        m[&typeid(otio::AnyDictionary)] = [](otio::any const& a, CxxAny* cxxAny) {
+        m[std::type_index(typeid(otio::AnyDictionary))] = [](otio::any const& a, CxxAny* cxxAny) {
             cxxAny->type_code = CxxAny::DICTIONARY;
             cxxAny->value.ptr = (void*) &otio::any_cast<otio::AnyDictionary const&>(a);
         };
@@ -106,7 +107,7 @@ struct _ToCxxAny {
 
 void otio_any_to_cxx_any(otio::any const& a, CxxAny* cxxAny) {
     static auto toCxxAny = _ToCxxAny();
-    auto e = toCxxAny.function_map.find(&a.type());
+    auto e = toCxxAny.function_map.find(std::type_index(a.type()));
     
     if (e != toCxxAny.function_map.end()) {
         e->second(a, cxxAny);
